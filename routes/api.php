2c2257cddbe3a1ef6eb2a1f13e7076f4523ebc89
Joki Game ML <jokigame@example.com>
2026-01-05 13:29:42 +0800

    izamajaa

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('provinces', ProvinceController::class);
     Route::apiResource('cities', CityController::class);
     Route::apiResource('districts', DistrictController::class);
     Route::apiResource('programs', ProgramController::class);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
